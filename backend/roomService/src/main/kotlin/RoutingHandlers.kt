a6df6abff78db9ca0c6b14ab0b6f58e55f720735
Joel Valerio <joelvalerio304@gmail.com>
2025-06-12 12:14:38 -0400

    Improvements to createRoomHandler

--- backend/roomService/src/main/kotlin/RoutingHandlers.kt
+++ backend/roomService/src/main/kotlin/RoutingHandlers.kt
@@ -3,20 +3,26 @@ package com.roomservice
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.ApplicationCall
 import io.ktor.server.response.respond
-import kotlinx.coroutines.future.await
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.future.asDeferred
+import kotlinx.serialization.Serializable
 import java.util.UUID
+
+@Serializable
+data class CreateRoomResponse(val playerID: String, val roomId: String)
 
 suspend fun createRoomHandler(call: ApplicationCall) {
     val redis = call.application.attributes[LETTUCE_REDIS_COMMANDS_KEY]
     val hostID = UUID.randomUUID().toString()
     val roomID = UUID.randomUUID().toString()
-    val hostFuture = redis.set(hostID, roomID)
-    val roomFuture = redis.lpush(roomID, hostID).await()
-    if (hostFuture.await() == "OK" && roomFuture == 1L) {
-        call.run {
-            application.environment.log.info("the room has been created")
-            call.respond(HttpStatusCode.OK)
-        }
+    val hostFuture = redis.set(hostID, roomID).asDeferred()
+    val roomFuture = redis.lpush(roomID, hostID).asDeferred()
+    val (hostStatus, roomStatus) = awaitAll(hostFuture, roomFuture)
+
+    if (hostStatus == "OK" && roomStatus == 1L) {
+        call.respond(HttpStatusCode.OK, CreateRoomResponse(hostID, roomID))
+    } else {
+        call.respond(HttpStatusCode.BadRequest, "Failed to create room")
     }
 }
 
