b5f74f40967d0039317c7696b5678bce0c5955bf
Joel Valerio <joelvalerio304@gmail.com>
2025-06-15 12:59:11 -0400

    Started LeaveRoom endpoint

--- backend/roomService/src/main/kotlin/routes/LeaveRoom.kt
+++ backend/roomService/src/main/kotlin/routes/LeaveRoom.kt
@@ -1,7 +1,27 @@
 package com.roomservice.routes
 
+import com.roomservice.Constants.PLAYER_TO_ROOM_PREFIX
+import com.roomservice.Constants.ROOM_TO_PLAYERS_PREFIX
+import com.roomservice.LETTUCE_REDIS_COMMANDS_KEY
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.ApplicationCall
+import io.ktor.server.response.respond
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.future.asDeferred
+import kotlinx.coroutines.future.await
 
 suspend fun leaveRoomHandler(call: ApplicationCall) {
-    call.application.environment.log.info("the room has been leaved")
+    val playerId = call.parameters["playerId"]
+        ?: return call.respond(HttpStatusCode.BadRequest)
+    val redis = call.application.attributes[LETTUCE_REDIS_COMMANDS_KEY]
+    val roomId = redis.get(PLAYER_TO_ROOM_PREFIX + playerId).await()
+
+    val lRemFuture = redis.lrem(ROOM_TO_PLAYERS_PREFIX + roomId, 1, playerId).asDeferred()
+    val delPlayerFuture = redis.del(PLAYER_TO_ROOM_PREFIX + roomId).asDeferred()
+    val delNameFuture = redis.del(PLAYER_TO_ROOM_PREFIX + playerId).asDeferred()
+
+    val futures = awaitAll(lRemFuture, delPlayerFuture, delNameFuture)
+    if (futures.any( {it -> it != 1L})) {
+
+    }
 }
