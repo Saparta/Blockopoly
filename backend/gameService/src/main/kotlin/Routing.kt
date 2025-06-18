2254202d7787a70b0c762506dd82cddf4dc22159
Joel Valerio <joelvalerio304@gmail.com>
2025-06-18 16:03:28 -0400

    Implemented working start endpoint

--- backend/gameService/src/main/kotlin/Routing.kt
+++ backend/gameService/src/main/kotlin/Routing.kt
@@ -14,14 +14,21 @@ fun Application.configureRouting() {
     routing {
         get("/") {call.respond(HttpStatusCode.OK)}
         post("/start/{roomId}") {
+            // TODO: Use JWT to authenticate request source + read playerId from request body
+            call.application.environment.log.info("Starting game")
             val redis = call.application.attributes[REDIS_COMMANDS_KEY]
             val roomId = call.parameters["roomId"] ?: return@post call.respond(HttpStatusCode.BadRequest)
+            val roomStartable = redis.llen(Constants.ROOM_TO_PLAYERS_PREFIX + roomId).await()
+            if (roomStartable < 2) {
+                call.respond(HttpStatusCode.BadRequest)
+            }
             val roomStarted = redis.setnx(Constants.ROOM_START_STATUS_PREFIX + roomId, "true")
             redis.expire(Constants.ROOM_START_STATUS_PREFIX + roomId, Constants.SECONDS_IN_DAY.toLong())
             if (!roomStarted.await()) {
                 return@post call.respond(HttpStatusCode.InternalServerError)
             }
             redis.publish(roomId, "START#")
+            call.respond(HttpStatusCode.OK)
         }
 
         route("/ws/play/{playerId}") {
