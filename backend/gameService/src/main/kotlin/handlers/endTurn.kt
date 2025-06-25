2f80abded0adcd32f68168cd6f9c42f07b1f6ba3
Joel Valerio <joelvalerio304@gmail.com>
2025-06-25 02:29:54 -0400

    Handled overflowing hands + card played counter reset at end of turn

--- backend/gameService/src/main/kotlin/handlers/endTurn.kt
+++ backend/gameService/src/main/kotlin/handlers/endTurn.kt
@@ -1,5 +1,7 @@
 package com.gameservice.handlers
 
+import com.gameservice.MAX_CARDS_PER_TURN
+import com.gameservice.models.Card
 import com.gameservice.models.GameState
 import kotlinx.coroutines.flow.MutableStateFlow
 
@@ -7,5 +9,14 @@ fun endTurn(game: MutableStateFlow<GameState>, playerId: String) : GameState {
     val current = game.value
     if (current.playerAtTurn != playerId) return current
     val nextPlayer = current.playerOrder[(current.playerOrder.indexOf(playerId) + 1) % current.playerOrder.size]
-    return current.copy(playerAtTurn = nextPlayer)
+    val removedCards = mutableListOf<Card>()
+    current.playerState[playerId]?.hand?.size?.let {
+        if (it > 7) {
+            while (current.playerState.getValue(playerId).hand.size > 7) {
+                removedCards.add(current.playerState.getValue(playerId).hand.removeLast())
+            }
+        }
+    }
+    current.discardPile.addAll(removedCards)
+    return current.copy(playerAtTurn = nextPlayer, cardsLeftToPlay = MAX_CARDS_PER_TURN, discardPile = current.discardPile)
 }
