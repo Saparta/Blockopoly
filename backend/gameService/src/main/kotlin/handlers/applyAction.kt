87a68055434f7efe854d2f7b8455a576e18a5577
Joel Valerio <joelvalerio304@gmail.com>
2025-06-25 16:26:41 -0400

    Players can now play Money cards

--- backend/gameService/src/main/kotlin/handlers/applyAction.kt
+++ backend/gameService/src/main/kotlin/handlers/applyAction.kt
@@ -3,6 +3,7 @@ package com.gameservice.handlers
 import com.gameservice.models.EndTurn
 import com.gameservice.models.GameAction
 import com.gameservice.models.GameState
+import com.gameservice.models.PlayMoney
 import com.gameservice.models.PlayProperty
 import com.gameservice.models.StartTurn
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -12,6 +13,7 @@ fun applyAction(game: MutableStateFlow<GameState>, playerId: String, action: Gam
     val newState = when (action) {
         is StartTurn -> startTurn(game, playerId)
         is PlayProperty -> playProperty(game, playerId, action)
+        is PlayMoney -> playForMoney(game, playerId, action)
         is EndTurn -> endTurn(game, playerId)
     }
     game.tryEmit(newState)
